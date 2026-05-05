fc1c57db3a95d1f60e9a92f43a75695b214c3dad
sahriar nur <shariarnurnahin99@gmail.com>
2026-05-05 12:35:02 -0600

    route, linting setup and adds necessary packages

---
+++ eslint.config.mjs
@@ -0,0 +1,6 @@
+import core from "ultracite/eslint/core";
+import react from "ultracite/eslint/react";
+import reactRefresh from "eslint-plugin-react-refresh"
+
+
+export default [...core, ...react, ...reactRefresh]
--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,19 @@ import { createRoot } from "react-dom/client"
 import "./index.css"
 import App from "./App.tsx"
 import { ThemeProvider } from "@/components/theme-provider.tsx"
+import { BrowserRouter } from "react-router-dom"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </BrowserRouter>
     </ThemeProvider>
   </StrictMode>
 )
